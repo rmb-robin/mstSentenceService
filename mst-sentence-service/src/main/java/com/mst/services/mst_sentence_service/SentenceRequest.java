3fd3015670c0bb56964d05283bfffed1141310a2
levk13 <l2kdevsolutions@gmail.com>
2017-04-24 12:40:36 -0500

    init commit..

---
+++ mst-sentence-service/src/main/java/com/mst/services/mst_sentence_service/SentenceRequest.java
@@ -0,0 +1,18 @@
+package com.mst.services.mst_sentence_service;
+
+
+import java.util.List;
+
+public class SentenceRequest {
+
+	private List<String> senteceTexts;
+
+	public List<String> getSenteceTexts() {
+		return senteceTexts;
+	}
+
+	public void setSenteceTexts(List<String> senteceTexts) {
+		this.senteceTexts = senteceTexts;
+	} 
+	
+}
