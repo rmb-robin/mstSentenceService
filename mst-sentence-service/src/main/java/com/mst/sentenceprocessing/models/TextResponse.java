b4bda92753a09565d2ca4d87adff5a7806a6d75d
levk13 <l2kdevsolutions@gmail.com>
2018-02-20 11:33:13 -0600

    added Model.edges.

---
+++ mst-sentence-service/src/main/java/com/mst/sentenceprocessing/models/TextResponse.java
@@ -0,0 +1,20 @@
+package com.mst.sentenceprocessing.models;
+
+public class TextResponse {
+
+	private String result;
+	private String message;
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
