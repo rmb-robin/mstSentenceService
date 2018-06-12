72746ead049c7a493b54d186a37cee3016c2578b
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-06-12 17:26:56 -0400

    clean up

--- mst-sentence-service/src/main/java/com/mst/sentenceprocessing/interfaces/RawReportService.java
+++ mst-sentence-service/src/main/java/com/mst/sentenceprocessing/interfaces/RawReportService.java
@@ -1,7 +1,6 @@
 package com.mst.sentenceprocessing.interfaces;
 
 import com.mst.model.HL7Details;
-import com.mst.model.discrete.DiscreteData;
 import com.mst.model.raw.AllHl7Elements;
 import com.mst.model.raw.ParseHl7Result;
 import com.mst.model.raw.RawReportFile;
@@ -11,8 +10,7 @@ import com.mst.sentenceprocessing.models.RawFileSaveResult;
 import ca.uhn.hl7v2.HL7Exception;
 
 public interface RawReportService {
-
 	RawFileSaveResult save(SentenceTextRequest request,RawReportFile file);
-	ParseHl7Result getSetentenceTextRequestFromRaw(HL7Details detail,RawReportFile file, AllHl7Elements allElements) throws HL7Exception;
+	ParseHl7Result getSentenceTextRequestFromRaw(HL7Details detail, RawReportFile file, AllHl7Elements allElements) throws HL7Exception;
 	String saveParsed(String rawFileId, SentenceTextRequest request);	
 }
