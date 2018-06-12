a5e81015bab557a63ca05a0d394d3db3e2e68141
Brian Sheely <bsheely@medicalsearchtechnologies.com>
2018-06-12 17:24:12 -0400

    new test class

--- mst-sentence-service/src/main/java/com/mst/test/RawReportControllerTest.java
+++ mst-sentence-service/src/main/java/com/mst/test/RawReportControllerTest.java
@@ -12,6 +12,7 @@ import com.mst.sentenceprocessing.models.RawFileSaveResult;
 import com.mst.sentenceprocessing.services.RawReportServiceImpl;
 import com.mst.sentenceprocessing.services.ReportsServiceImpl;
 import com.mst.sentenceprocessing.services.SentenceServiceImpl;
+import org.bson.types.ObjectId;
 import org.junit.Test;
 
 import java.util.List;
@@ -30,14 +31,60 @@ public class RawReportControllerTest {
     }
 
     @Test
-    public void saveRawFile() {
-        RawReportFile file = getRawReportFile();
+    public void saveRawFile1() {
+        RawReportFile file = new RawReportFile();
+        file.setId(new ObjectId());
+        file.setContent(getTest1());
+        file.setProcessed(false);
+        file.setOrgId("58c6f3ceaf3c420b90160803");
+        file.setOrgName("rad");
+        file.setIsProcessingtypeSentenceDiscovery(false);
+        processRawFile(file);
+    }
+
+    @Test
+    public void saveRawFile2() {
+        RawReportFile file = new RawReportFile();
+        file.setId(new ObjectId());
+        file.setContent(getTest2());
+        file.setProcessed(false);
+        file.setOrgId("58c6f3ceaf3c420b90160803");
+        file.setOrgName("rad");
+        file.setIsProcessingtypeSentenceDiscovery(false);
+        processRawFile(file);
+    }
+
+    @Test
+    public void saveRawFile3() {
+        RawReportFile file = new RawReportFile();
+        file.setId(new ObjectId());
+        file.setContent(getTest3());
+        file.setProcessed(false);
+        file.setOrgId("58c6f3ceaf3c420b90160803");
+        file.setOrgName("rad");
+        file.setIsProcessingtypeSentenceDiscovery(false);
+        processRawFile(file);
+    }
+
+    @Test
+    public void saveRawFile4() {
+        RawReportFile file = new RawReportFile();
+        file.setId(new ObjectId());
+        file.setContent(getTest4());
+        file.setProcessed(false);
+        file.setOrgId("58c6f3ceaf3c420b90160803");
+        file.setOrgName("rad");
+        file.setIsProcessingtypeSentenceDiscovery(false);
+        processRawFile(file);
+    }
+
+    private void processRawFile(RawReportFile file) {
         try {
             List<HL7Details> detailsList = reportsService.getHL7DetailsByOrgName(file.getOrgName());
             assertTrue(!detailsList.isEmpty());
             HL7Details details = detailsList.get(0);
             AllHl7Elements allHl7Elements = reportsService.getAllHl7Elements();
-            ParseHl7Result parsedResult = rawReportService.getSetentenceTextRequestFromRaw(details, file, allHl7Elements);
+            ParseHl7Result parsedResult = rawReportService.getSentenceTextRequestFromRaw(details, file, allHl7Elements);
             SentenceTextRequest request = parsedResult.getSentenceTextRequest();
             request.getDiscreteData().setOrganizationId(file.getOrgId());
             request.getDiscreteData().setAllAvailableFields(parsedResult.getAllFields());
@@ -47,7 +94,6 @@ public class RawReportControllerTest {
             request.getDiscreteData().setRawFileId(fileId);
             String parsedId = rawReportService.saveParsed(fileId, request);
             request.getDiscreteData().setParseReportId(parsedId);
-            assertTrue(parsedResult.getMissingFields().isEmpty());
             request.getDiscreteData().getAllAvailableFields().clear();
             sentenceService.processSentenceTextRequest(request);
         } catch (Exception ex) {
@@ -55,13 +101,25 @@ public class RawReportControllerTest {
         }
     }
 
-    private RawReportFile getRawReportFile() {
-        RawReportFile file = new RawReportFile();
-        file.setContent("MSH|^~\\\\&|PSCRIBE|SDI-AZELOC_00431_AC|RADIS1|BMG|20180320214120||ORU^R01|9624447|P|2.3|20084623\\\\rPID|1|324793703|02049554||SPENCE^PAMELA^M||19830530|F||||||||||AZE-8195379|629011955\\\\rPV1|1|O|AZELOC_00431_AC|||||1023422532^BOTTOMLEY^WILLIAM^|||||||||||AZE-8195379|||||||||||||||||||||||||20180320152807\\\\rORC|RE|10995017459||||\\\\rOBR|1|10995017459|SDI-10995017459|3266114633^IH Hip W+orW/O Pelvis 2-3 vws Lt|||201803201642|||||||||1023422532^BOTTOMLEY^WILLIAM||||||20180320213711||XA|F||^^^201803201642||||^left hip pain||RRWJACOBY^Jacoby||^^20180320213619\\\\rOBX|1|FT|3266114633^IH Hip W+orW/O Pelvis 2-3 vws Lt||ZZZCOMPARISON:  None.ZZZZZZHISTORY:  left hip pain.ZZZZZZTECHNIQUE:  Pelvis and left hipZZZZZZFINDINGS:  Negative for acute fracture or malalignment. ZZZZZZIMPRESSION:  Negative for fracture. ZZZZZZInterpreted By: William Jacoby, M.D.ZZZ Signature Date:3/20/2018 9:37 PM MST ZZZZZZThe workstation used in generating this report was CRIPHX113944570.ZZZZZZ||||||F|||20180320213711||RRWJACOBY^Jacoby\\\\r");
-        file.setProcessed(false);
-        file.setOrgId("58c6f3ceaf3c420b90160803");
-        file.setOrgName("rad");
-        file.setIsProcessingtypeSentenceDiscovery(false);
-        return file;
+    private String getTest1() { //2.3
+        return "MSH|^~\\&||MG||Martin General|20180531000042||ORU^R01|2660359|P|2.3|29096256|||||||\n" +
+                "PID|||47023MG||RAHMAN^JEAN^R||19390309|F|||PO BOX 1555^1107 FEGGINS ROAD^ROBERSONVILLE^NC^27871||(252)916-2242|||||5642184^^^20180530225200||||||||||||\n" +
+                "PV1||O|ED^E108^01^MG||||536^ROCCI^CHARLES^^^^ALLEN|536^ROCCI^CHARLES^^^^ALLEN|||||||||536^ROCCI^CHARLES^^^^ALLEN|E|5642184|||||||||||||||||||||||||20180530223200||||||||\n" +
+                "ORC|RE|||||||||||536^ROCCI^CHARLES^^^^ALLEN|||||MG||\n" +
+                "OBR|1|56421840000200MG|56421840000200MG|RPID6415^XR KNEE 3 VIEWS|||20180530225200|||||||||536^ROCCI^CHARLES^^^^ALLEN||56421840000200MG||1||20180530235935||CR|F|F|^^^20180530225200||||Edema|1710195276^Hendrix^Christopher|1710195276^Hendrix^Christopher||||||||||\n" +
+                "ZPS||||{\"pid3\":\"47023\",\"ord\":\"200\",\"accession\":\"56421840000200\",\"obr41\":\"KNEE\",\"obr42\":\"LE-KNEE 3V CR\",\"obr43\":\"RAD\",\"obr276\":\"S\"}|20180530235935|||||\n" +
+                "OBX|1|FT|RPID6415&BODY||EXAM DESCRIPTION: Right knee, 3 views    CLINICAL HISTORY: Edema    COMPARISON: None.    FINDINGS: 3 views of the right knee. No acute fracture or  dislocation. Normal osseous mineralization. Moderate 3  compartment joint space narrowing and marginal osteophytosis.  Moderate joint effusion.    IMPRESSION:    1. No acute fracture identified.    2. Moderate joint effusion.    3. Moderate 3 compartment osteoarthritic change.      Electronically signed by:  Christopher Hendrix  5/30/2018 11:59  PM CDT Workstation: 103-1151||||||F";
+    }
+
+    private String getTest2() {
+        return "";
+        }
+
+    private String getTest3() {
+        return "";
+    }
+
+    private String getTest4() {
+        return "";
     }
 }
